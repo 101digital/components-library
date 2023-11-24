f6c3d8697b9a458e91cd0ce9dc7063a95b59b382
sharmal perera <sharmal@101digital.io>
2023-11-24 13:59:34 +1100

    library file update

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 const userProfileFunctions = require('./libraries/user-profile-component/user-profile-form-template.js');
 const userProfileSummaryFunctions = require('./libraries/user-profile-component/user-profile-summary-template.js');
 const financialProfileSummaryFunctions = require('./libraries/financial-profile-componenet/financial-profile-summary-template.js');
+const buttonGroupFunctions = require('./libraries/template-componenet/button-group-template.js');
 
 exports.getDataByComponentId = id => {
   switch (id) {
-    case "user-profile-form-component":
+    case "user-profile-form-template":
       return userProfileFunctions;
-    case "user-profile-summary-component":
+    case "user-profile-summary-template":
       return userProfileSummaryFunctions;
-    case "financial-profile-summary-component":
+    case "financial-profile-summary-template":
       return financialProfileSummaryFunctions;
+    case "button-group-template":
+      return buttonGroupFunctions;
     default:
       break;
   }
