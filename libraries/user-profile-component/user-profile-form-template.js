55312fb22e089606a8e51c15ddd3e198460115bf
Hua Cat Luong <hualuong23596@gmail.com>
2023-12-06 10:59:41 +0700

    Update template

--- libraries/user-profile-component/user-profile-form-template.js
+++ libraries/user-profile-component/user-profile-form-template.js
@@ -355,6 +355,7 @@ const ReturnStatement = (fields,enableTranslation) => {
     }
 
     switch (field.type) {
+      case 'title':
       case 'subtitle':
         componentCode += `{/* ${field.type} - ${field.label} */}
         <View
